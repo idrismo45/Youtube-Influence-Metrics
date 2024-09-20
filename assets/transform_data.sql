219285d98e9f732dbc08801b7d4c4774f38f662b
Mohamed Idris <144534902+idrismo45@users.noreply.github.com>
2024-09-20 12:01:22 +0100

    Update transform_data.sql

--- assets/transform_data.sql
+++ assets/transform_data.sql
@@ -1,1 +1,15 @@
 
+/*
+# 1. Select the required columns
+# 2. Extract the channel name from the 'NOMBRE' column
+*/
+
+-- 1.
+SELECT
+    SUBSTRING_INDEX(NOMBRE, '@', 1) AS channel_name,  -- 2.
+    total_subscribers,
+    total_views,
+    total_videos
+
+FROM
+    top_uk_youtubers_2024;
